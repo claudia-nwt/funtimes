4d5c58715220bfaf419577082a3e7cd217cc4989
claudia-nwt <haas8400@mylaurier.ca>
2024-04-03 13:33:13 -0600

    The truth is added

---
+++ LetsParty.R
@@ -0,0 +1,11 @@
+#I'm not really sure what I should do here.
+#But it's important to document what you're doing, even if you don't now what that is
+
+x=1
+y=2
+
+x+y
+
+#yet that isn't the truth
+
+z=(x+y)
